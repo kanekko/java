999aadb30e01ddfa4de0eccb40a7dc8b341ce03b
Canek García <kanect@rocketmail.com>
2016-07-05 12:48:04 -0500

    CAritmetcia whit three variables

--- Cap01/CAritmetica.java
+++ Cap01/CAritmetica.java
@@ -4,22 +4,23 @@ class CAritmetica
 	 * Operaciones aritméticas
 	 */
 	public static void main(String[] args) {
-		int dato1, dato2, resultado;
+		int dato1, dato2, dato3, resultado;
 
 		dato1 = 20;
 		dato2 = 10;
+		dato3 = 5;
 
 		// Suma
-		resultado = dato1 + dato2;
-		System.out.println(dato1 + " + " + dato2 + " = " + resultado);
+		resultado = dato1 + dato2 + dato3;
+		System.out.println(dato1 + " + " + dato2 + " + " + dato3 + " = " + resultado);
 
 		// Resta
-		resultado = dato1 - dato2;
-		System.out.println(dato1 + " - " + dato2 + " = " + resultado);
+		resultado = dato1 - dato2 - dato3;
+		System.out.println(dato1 + " - " + dato2 + " - " + dato3 + " = " + resultado);
 
 		// Producto
-		resultado = dato1 * dato2;
-		System.out.println(dato1 + " * " + dato2 + " = " + resultado);
+		resultado = dato1 * dato2 * dato3;
+		System.out.println(dato1 + " * " + dato2 + " * " + dato3 + " = " + resultado);
 
 		// Cociente
 		resultado = dato1 / dato2;
