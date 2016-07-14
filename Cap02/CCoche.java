67d2a813cddc22e6e5d3db2614eb951f3853a5d5
Canek García <kanect@rocketmail.com>
2016-07-14 10:42:54 -0500

    clase CCoche que representa coches.

---
+++ Cap02/CCoche.java
@@ -0,0 +1,69 @@
+class CCoche
+{
+	String Marca;
+	String Modelo;
+	String Color;
+	boolean MotorEncendido;
+	int PalancaDeCambios;
+	boolean PedalAcelerador;
+	boolean PedalFreno;
+	
+	void ArrancarMotor()
+	{
+		if(MotorEncendido == true)
+			System.out.println("El motor ya está encendido.");
+		else
+		{
+			MotorEncendido = true;
+			System.out.println("El motor se ha encendido.");
+		}
+	}
+	
+	void CambiarVelocidad()
+	{
+		if(PedalAcelerador == true)
+			PalancaDeCambios = PalancaDeCambios + 1;
+		if(PedalFreno == true)
+			PalancaDeCambios = PalancaDeCambios - 1;
+		
+		System.out.println("PalancaDeCambios:~>" + PalancaDeCambios);
+	} 
+	
+	void Acelerar()
+	{
+		PedalAcelerador = true;
+		PedalFreno = false;
+		CambiarVelocidad();
+	}
+	
+	void Frenar()
+	{
+		PedalAcelerador = false;
+		PedalFreno = true;
+		CambiarVelocidad();
+	}
+	
+	void PararMotor()
+	{
+		if(MotorEncendido == true)
+		{
+			MotorEncendido = false;
+			System.out.println("El motor se ha parado.");
+		}
+		else
+			System.out.println("El motor ya está parado.");
+	}
+	
+	
+	public static void main(String[] args){
+		CCoche MiCoche = new CCoche();
+		MiCoche.Marca = "Ford";
+		MiCoche.Modelo = "Mustang";
+		MiCoche.Color = "Negro";
+		MiCoche.PalancaDeCambios = 0;
+		MiCoche.ArrancarMotor();
+		MiCoche.Acelerar();
+		MiCoche.Frenar();
+		MiCoche.PararMotor();
+	}
+}
